12b7a6eff540e15f3fb43ede69f4940b4df229c9
sambbaahh <samikukkonen00@hotmail.com>
2024-02-24 22:48:40 +0200

    learning nodeJS "net" aka socket

--- server/index.js
+++ server/index.js
@@ -1,5 +1,30 @@
-import  http  from "http"
+import net from "net";
+let socketConnections = [];
 
-http.createServer((req, res) => {
-  console.log("pöö")
-}).listen(3000);
+//create tcp server
+const server = net.createServer((socket) => {
+  socketConnections.push(socket);
+
+  socket.on("data", data => {
+    socketConnections.forEach(connection => {
+      if(socket !== connection){
+        connection.write(data)
+      }
+    });
+  })
+
+  socket.on("close", () => {
+    // Remove closed socket connection
+    const index = socketConnections.indexOf(socket);
+    if (index !== -1) {
+      socketConnections.splice(index, 1);
+    }
+  });
+  
+});
+
+
+//listen port 3000
+server.listen(3000, () => {
+  console.log("Server started");
+})
