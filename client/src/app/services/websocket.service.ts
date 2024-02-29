ae858c39697b4c8e5ba3831c4bcb1f51a0ce026a
sambbaahh <samikukkonen00@hotmail.com>
2024-02-29 14:48:09 +0200

    services

--- client/src/app/services/websocket.service.ts
+++ client/src/app/services/websocket.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { WebSocketSubject, webSocket } from 'rxjs/webSocket';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebsocketService {
+  private url: string = 'localhost:3000';
+  private subject$: WebSocketSubject<unknown>;
 
-  constructor() { }
+  constructor() {
+    this.subject$ = webSocket(this.url);
+  }
+
+  sendMessage(message: string) {
+    this.subject$.next('pöö');
+  }
+
+  getMessages() {
+    this.subject$.asObservable();
+  }
 }
