2e8c9291795445ecff225d23683ec680a78e2cff
sambbaahh <samikukkonen00@hotmail.com>
2024-02-29 11:15:27 +0200

    rename

---
+++ client/src/app/lobby/lobby.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lobby',
+  standalone: true,
+  imports: [],
+  templateUrl: './lobby.component.html',
+  styleUrl: './lobby.component.css'
+})
+export class LobbyComponent {
+
+}
