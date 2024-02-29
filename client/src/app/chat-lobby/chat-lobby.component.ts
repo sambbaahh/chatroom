295ea071aec378a59ed02760ba3c57541cc017fd
sambbaahh <samikukkonen00@hotmail.com>
2024-02-29 11:12:45 +0200

    new components

---
+++ client/src/app/chat-lobby/chat-lobby.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-lobby',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-lobby.component.html',
+  styleUrl: './chat-lobby.component.css'
+})
+export class ChatLobbyComponent {
+
+}
