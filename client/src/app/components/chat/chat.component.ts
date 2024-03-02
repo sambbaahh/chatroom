66b667ce52394272ec8f174ea6f681d2934aaf61
sambbaahh <samikukkonen00@hotmail.com>
2024-03-02 13:23:42 +0200

    creating chat component

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, ButtonModule, InputGroupModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
