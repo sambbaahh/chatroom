d48413e58281e63ae171a8adaa89c0f486885764
sambbaahh <samikukkonen00@hotmail.com>
2024-04-18 22:13:05 +0300

    creating login page

--- client/src/app/components/landing-page/landing-page.component.ts
+++ client/src/app/components/landing-page/landing-page.component.ts
@@ -1,46 +1,38 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { TabViewModule } from 'primeng/tabview';
+import { PasswordModule } from 'primeng/password';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { WebsocketService } from '../../services/websocket.service';
-import { NewUser, RequestEnum } from '../../interfaces';
-import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'landing-page',
   standalone: true,
-  imports: [FormsModule, ButtonModule, InputTextModule],
+  imports: [
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    PasswordModule,
+    TabViewModule,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent {
-  public newUsername: string = '';
-  public newUserId: string = uuidv4();
-  private storageUsername: string | null = localStorage.getItem('username');
+  public username: string = '';
+  public password: string = '';
 
-  constructor(
-    private router: Router,
-    public webSocketService: WebsocketService
-  ) {
-    if (this.storageUsername) {
-      this.newUsername = this.storageUsername;
-    }
+  constructor(private router: Router) {}
+
+  onSubmitLogin(): void {
+    this.router.navigate(['/lobby']);
   }
-
-  onSubmit(): void {
-    const newUser: NewUser = {
-      type: RequestEnum.NEW_USER,
-      username: this.newUsername,
-      userId: this.newUserId,
-    };
-
-    this.webSocketService.sendMessage(JSON.parse(JSON.stringify(newUser)));
-    this.webSocketService.username = this.newUsername;
-    this.webSocketService.userId = this.newUserId;
-    localStorage.setItem('username', this.newUsername);
-    localStorage.setItem('userId', this.newUserId);
-
+  onSubmitRegister(): void {
     this.router.navigate(['/lobby']);
   }
 }
