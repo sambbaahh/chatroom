aaa156899e15fb3aaa6903db614811150b48d1aa
sambbaahh <samikukkonen00@hotmail.com>
2024-04-22 16:15:01 +0300

    developing chat ui

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -7,6 +7,7 @@ import {
   Container,
   Button,
   TextInput,
+  Avatar,
 } from '@mantine/core';
 import { getHotkeyHandler } from '@mantine/hooks';
 import { IconMessages, IconSend } from '@tabler/icons-react';
@@ -38,7 +39,67 @@ export default function Chat({ roomName }: Props) {
   );
 }
 
+const messages = [
+  {
+    username: 'sami',
+    message: 'Ensimmäinen viesti!',
+    timestamp: '2024-04-22T12:00:00Z',
+  },
+  {
+    username: 'iida',
+    message: 'Toinen viesti.',
+    timestamp: '2024-04-22T12:05:00Z',
+  },
+  {
+    username: 'sami',
+    message: 'Ensimmäinen viesti!',
+    timestamp: '2024-04-22T12:00:00Z',
+  },
+  {
+    username: 'sami',
+    message: 'Ensimmäinen viesti!',
+    timestamp: '2024-04-22T12:00:00Z',
+  },
+  {
+    username: 'hiauu',
+    message: 'Kolmas viesti tässä.',
+    timestamp: '2024-04-22T12:10:00Z',
+  },
+  {
+    username: 'iida',
+    message: 'Toinen viesti.',
+    timestamp: '2024-04-22T12:05:00Z',
+  },
+  {
+    username: 'iida',
+    message: 'Toinen viesti.',
+    timestamp: '2024-04-22T12:05:00Z',
+  },
+  {
+    username: 'hiauu',
+    message: 'Kolmas viesti tässä.',
+    timestamp: '2024-04-22T12:10:00Z',
+  },
+  {
+    username: 'iida',
+    message: 'Toinen viesti.',
+    timestamp: '2024-04-22T12:05:00Z',
+  },
+  {
+    username: 'iida',
+    message: 'Toinen viesti.',
+    timestamp: '2024-04-22T12:05:00Z',
+  },
+  {
+    username: 'hiauu',
+    message: 'Kolmas viesti tässä.',
+    timestamp: '2024-04-22T12:10:00Z',
+  },
+];
+
 function ChatChat({ roomName }) {
+  const me = 'sami';
+
   return (
     <>
       <Box
@@ -51,6 +112,45 @@ function ChatChat({ roomName }) {
         <Button> close </Button>
         <Text>{roomName}</Text>
         <Button> exit </Button>
+      </Box>
+      <Box
+        style={{
+          height: '100%',
+          overflow: 'scroll',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        {messages.map((message, index, allMessages) => (
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: '1rem',
+            }}
+          >
+            {message.username !== me &&
+              index < allMessages.length &&
+              allMessages[index - 1].username !== message.username && (
+                <Avatar>{message.username.charAt(0)}</Avatar>
+              )}
+            <Box
+              style={{
+                marginLeft: message.username === me ? 'auto' : '0px',
+                alignItems: message.username === me ? 'end' : 'start',
+                backgroundColor: 'lightgray',
+                marginBlock: '0.5rem',
+                borderRadius: '1rem',
+                padding: '1rem',
+                width: 'fit-content',
+                maxWidth: '75%',
+              }}
+            >
+              <Text variant="h1">{message.message}</Text>
+            </Box>
+          </Box>
+        ))}
       </Box>
       <Box style={{ marginTop: 'auto', marginBottom: '0.5rem' }}>
         <TextInput
