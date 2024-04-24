720d47139dc407119e54d9ba0ad085a35e5ac901
sambbaahh <samikukkonen00@hotmail.com>
2024-04-24 19:36:31 +0300

    save frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <MantineProvider>
+        <MantineProvider defaultColorScheme="auto">
           <App />
         </MantineProvider>
       </AuthProvider>
